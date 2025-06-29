59a00e0ef936fc9d1718af76a47598d2580b7517
Yourixf <Yourivanbeuzekom@upcmail.nl>
2025-06-30 00:03:15 +0200

    base commit

---
+++ src/main/java/nl/yourivb/TicketTrack/TicketTrackApplication.java
@@ -0,0 +1,13 @@
+package nl.yourivb.TicketTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/yourivb/TicketTrack/TicketTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.yourivb.TicketTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
